18cb85d7bbc0324b1b68c07919d63218dd015c8c
JIYUN KIM <91119322+JiYun1101@users.noreply.github.com>
2025-06-21 00:30:02 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 resource "aws_instance" "web" {
   ami           = "ami-00c39f71452c08778"
   instance_type = "t3.micro"
